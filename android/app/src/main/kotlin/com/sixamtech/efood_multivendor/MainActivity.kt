bbff96b903d784fb5057cc62980749a6b80454d4
ZamuelMM <samuel.mastermovers@gmail.com>
2023-09-28 16:51:20 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/sixamtech/efood_multivendor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mastermovers.buyer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme/light_theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+ThemeData light({Color color = const Color(0xFFE33530)}) => ThemeData(
+      fontFamily: 'Roboto',
+      primaryColor: color,
+      secondaryHeaderColor: const Color(0xFFCB6C6C),
+      disabledColor: const Color(0xFFBABFC4),
+      brightness: Brightness.light,
+      hintColor: const Color(0xFF9F9F9F),
+      cardColor: Colors.white,
+      textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(foregroundColor: color)),
+      colorScheme: ColorScheme.light(primary: color, secondary: color)
+          .copyWith(background: const Color(0xFFF3F3F3))
+          .copyWith(error: const Color(0xFFE84D4F)),
+    );
